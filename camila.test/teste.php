3553e8138180b32c52476fcb36e74b3097412144
Camila do Amaral Ferreira <camila.2023301840@aluno.iffar.edu.br>
2025-03-31 14:56:40 -0300

    Add files via upload

---
+++ camila.test/teste.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+   <form method="post" action="formulario.php">
+
+   Digite o seu nome:
+
+   <input type="text" size="60" name="txtnome">
+   <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
