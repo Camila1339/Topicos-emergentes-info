49d225c3eeb7bfa08895fbd1dc3dff1e9e016ece
Camila do Amaral Ferreira <camila.2023301840@aluno.iffar.edu.br>
2025-05-08 16:12:47 -0300

    Update conexao.php

--- Agenda/conexao.php
+++ Agenda/conexao.php
@@ -1,23 +1,12 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Contatos</h1>
-        <form action=index.php method= "post">
+<?php
+$servidor = "localhost";
+$user = "root";
+$senha = " ";
+$banco= "agenda";
 
-         Nome:<input type="text" name="nome">
-        <br>
-         Endereço: <input type="text" name="endereco">
-        <br>
-         Telefone: <input type="text" name="telefone">
-        <br>
-         <input type="submit" value=Cadastrar>
-
-         </form>
-</body>
-</html>
+$conexao = mysqli_connect($servidor, $user, $senha, $banco);
+if (!$conexao){
+    echo "erro de conexão";
+}
+?>
